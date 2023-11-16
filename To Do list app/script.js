bf42e3ade80119d5005897e57e721ba183d90702
Stephen <christianralphdelacruz@gmail.com>
2023-11-16 19:26:11 -0300

    16-11-2023

--- To Do list app/script.js
+++ To Do list app/script.js
@@ -9,6 +9,30 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML = inputbox.value;
         listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
     inputbox.value = "";
+    saveData();
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+    
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
