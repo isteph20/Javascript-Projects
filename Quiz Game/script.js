d53d95be9e6fa9b59a16e1f25523604158d3c1b4
Stephen <christianralphdelacruz@gmail.com>
2023-11-17 13:09:30 -0300

    17-11-2023

---
+++ Quiz Game/script.js
@@ -0,0 +1,11 @@
+/* function deposit() {
+    return 1
+} */
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    const depositAmount = prompt("Enter amount: ");
+}
+
+deposit();
